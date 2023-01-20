1bbb5025e9a6ab7871246992e57108597a7a5801
Kadã <dgames201615@outlook.com>
2023-01-20 17:35:27 -0300

    defining global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
 	return (
-		<h1>Hello World!</h1>
+		<>
+			<GlobalStyles />
+			<h1>Hello World!</h1>
+		</>
 	);
 }
 
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+
+    font-size: 16px;
+    font-family: 'Inter', sans-serif;
+
+    background: #D2D2D2;
+  }
+`;
