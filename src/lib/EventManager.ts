ee819ef2bc985fd523bd8756d1b66b1f3df6152b
Kadã <dgames201615@outlook.com>
2023-02-07 20:11:07 -0300

    implementing the event listeners removal method

--- src/lib/EventManager.ts
+++ src/lib/EventManager.ts
@@ -43,15 +43,39 @@ export default class EventManager {
 			listener(payload);
 		});
 	}
+
+	removeListener({ event, listener: ListenerToRemove }: OnProps) {
+		const listeners = this.listeners[event];
+
+		if (!listeners) {
+			return;
+		}
+
+		const filteredListeners = listeners.filter(
+			(listener) => listener !== ListenerToRemove
+		);
+
+		this.listeners[event] = filteredListeners;
+	}
+}
+
+function addToast(payload: ListenerProps) {
+	console.log('listener1', payload);
+}
+
+function addToast2(payload: ListenerProps) {
+	console.log('listener2', payload);
 }
 
 const toastEventManagar = new EventManager();
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-toastEventManagar.on({ event: 'addtoast', listener: (payload: ListenerProps) => {
-	console.log('listener1', payload);
-}});
+toastEventManagar.on({ event: 'addtoast', listener: addToast});
+
+toastEventManagar.on({ event: 'addtoast', listener: addToast2});
 
 toastEventManagar.emit({ event: 'addtoast', payload: { type: 'error', text: 'Error' }});
+
+toastEventManagar.removeListener({event: 'addtoast', listener: addToast });
 
 console.log(toastEventManagar);
